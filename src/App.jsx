d62276301fdad787e5c980131c6aa3dafb0ff739
codewithmike-jpg <mikeyxofficial1234@gmail.com>
2025-05-11 21:57:44 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+      <h1 className='text-red-500 text-4xl text-center font-extrabold'>Hey its me IFearAids From Devsentinel</h1>
+
+      <button className='p-6 bg-red-600 text-bold cursor-pointer text-white mt-12 font-extrabold'>Click Here to shutdown this website forever</button>
+
+      <button className='p-6 bg-blue-600 text-bold cursor-pointer text-white mt-12 font-extrabold'>Click Here to restore all contents </button>
+      { /*if you are seeing this , i was just goofing about ;)  */}
+    </div>
+  )
+}
+
+export default App
